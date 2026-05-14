583dce87795bf786090b370d335772551f749bfe
Rohit Singh <singhrohit004008@gmail.com>
2026-05-14 16:35:56 +0530

    display and add method done

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        StudentManagement sm = new StudentManagement();
+
+        while(true) {
+
+            System.out.println("1. Add Student");
+            System.out.println("2. Display Students");
+            System.out.println("3. Exit");
+
+            System.out.print("Enter choice: ");
+            int choice = sc.nextInt();
+
+            switch(choice) {
+
+                case 1:
+
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+
+                    sc.nextLine();
+
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter Age: ");
+                    int age = sc.nextInt();
+
+                    Student s = new Student(id, name, age);
+
+                    sm.addStudent(s);
+
+                    break;
+
+                case 2:
+                    sm.displayStudents();
+                    break;
+
+                case 3:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        }
+    }
+}
--- StudentManagement.java
+++ StudentManagement.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
 public class StudentManagement {
-    
+    ArrayList<Student> students = new ArrayList<>();
+
+    public void addStudent(Student s) {
+        students.add(s);
+        System.out.println("student add sucesfully");
+    }
+
+    public void displayStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No Students Found");
+            return;
+        }
+        for(Student s : students) {
+            s.Display();
+        }
+    }
 }
